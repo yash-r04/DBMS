ddd5af3019d4f6b7c8a6ea25df458576da8370cb
yashr <yashaswinirao12@gmail.com>
2025-11-12 18:40:06 +0530

    changes in the model

--- laby/equipment/models.py
+++ laby/equipment/models.py
@@ -24,15 +24,16 @@ class Supplier(models.Model):
         return self.name
 
 class Equipment(models.Model):
-    supplier = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True, related_name='equipments')
     name = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
-    quantity = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    condition = models.CharField(max_length=50, blank=True)
+    category = models.CharField(max_length=50)
+    quantity = models.IntegerField()
     location = models.CharField(max_length=100)
+    condition = models.CharField(max_length=50, default='Good')
     added_on = models.DateField(auto_now_add=True)
-
+    description = models.TextField(blank=True, help_text="Short description or usage of the equipment")
+    datasheet = models.FileField(upload_to='datasheets/', blank=True, null=True)
+    image = models.ImageField(upload_to='equipment_images/', blank=True, null=True)
+    
     def __str__(self):
         return self.name
 
